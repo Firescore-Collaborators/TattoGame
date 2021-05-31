de25bfef7ce90bb53d5793e2e1dad191b7a8e2c9
cshoaib05 <choudharyshoaib05@gmail.com>
2021-05-31 13:28:43 +0530

    added

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject LookPos;
+    public GameObject Character;
+    void Start()
+    {
+        StartCoroutine(LookatPos());   
+    }
+    
+    IEnumerator LookatPos()
+    {
+        yield return new WaitForSeconds(0.5f);
+        LeanTween.rotate(Character, new Vector3(0, 180f, 0), 0.5f);
+    }
+
+}
