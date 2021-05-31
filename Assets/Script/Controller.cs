a7e5a7d4dd9977c1349c5e3719f51e3d4885c246
cshoaib05 <choudharyshoaib05@gmail.com>
2021-05-31 20:16:06 +0530

    added

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -6,6 +6,9 @@ public class Controller : MonoBehaviour
 {
     public GameObject LookPos;
     public GameObject Character;
+    public GameObject ObjectTatoo;
+    public GameObject camera;
+    public GameObject TattoColor;
     void Start()
     {
         StartCoroutine(LookatPos());   
@@ -15,6 +18,20 @@ public class Controller : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         LeanTween.rotate(Character, new Vector3(0, 180f, 0), 0.5f);
+        yield return new WaitForSeconds(3f);
+        LeanTween.move(camera, camera.transform.forward * 3.2f, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        ObjectTatoo.SetActive(true);
     }
 
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            TattoColor.SetActive(true);
+            TattoColor.transform.position = Input.mousePosition;
+            TattoColor.transform.localScale = TattoColor.transform.localScale *1.1f;
+        }
+    }
 }
