a120049566d9b2992174952869d21f6b20cfa729
cshoaib05 <choudharyshoaib05@gmail.com>
2021-06-13 02:04:36 +0530

    added

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class Controller : MonoBehaviour
     public GameObject ObjectTatoo;
     public GameObject camera;
     public GameObject TattoColor;
+    public GameObject CameraLastPos;
+    public GameObject Tatto;
+    public GameObject Line;
+    public GameObject Fill;
+
+
     void Start()
     {
         StartCoroutine(LookatPos());   
@@ -19,19 +26,32 @@ public class Controller : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         LeanTween.rotate(Character, new Vector3(0, 180f, 0), 0.5f);
         yield return new WaitForSeconds(3f);
-        LeanTween.move(camera, camera.transform.forward * 3.2f, 0.5f);
-        yield return new WaitForSeconds(0.5f);
-        ObjectTatoo.SetActive(true);
+        LeanTween.move(camera,CameraLastPos.transform.position, 0.5f);
+        yield return new WaitForSeconds(0.4f);
+        Character.SetActive(false);
+        // ObjectTatoo.SetActive(true);
+        yield return new WaitForSeconds(0.4f);
+        Tatto.SetActive(true);
     }
 
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             TattoColor.SetActive(true);
-            TattoColor.transform.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            TattoColor.SetActive(true); 
             TattoColor.transform.localScale = TattoColor.transform.localScale *1.1f;
         }
+    }
+
+    public void ChangeColor(Image spriteColor)
+    {
+        Line.GetComponent<LineRenderer>().sharedMaterial.color = spriteColor.color;
+        Fill.GetComponent<SpriteRenderer>().color = spriteColor.color;
     }
 }
---
+++ Assets/Script/LineDraw.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour
+{
+    public GameObject LinePrefab;
+    public GameObject currentLine;
+
+    public LineRenderer lineRenderer;
+
+    public List<Vector3> fingerPositions;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+           CreateLine();
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 tempfingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, 0f, 0.31f));
+            UpdateLine(tempfingerPos);
+            
+        }
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(LinePrefab, Vector3.zero, Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        fingerPositions.Clear();
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f,0f,0.31f)));
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, 0f, 0.31f)));
+        lineRenderer.SetPosition(0 , fingerPositions[0]);
+        lineRenderer.SetPosition(1 , fingerPositions[1]);
+
+    }
+
+    void UpdateLine(Vector3 newFingerPos)
+    {
+        fingerPositions.Add(newFingerPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newFingerPos);
+    }
+
+}
